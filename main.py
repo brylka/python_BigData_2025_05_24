80fb93a6e0d74ad7c0af24f9a122f751b624157d
Bartosz Bryniarski <brylka@jelenia.com>
2025-05-24 13:45:20 +0200

    sobota

--- main.py
+++ main.py
@@ -63,28 +63,64 @@ zmienna4 = True         # bool - prawda/fałsz True False
 # kamil = "    Kamil Ślimak"
 # print(":" + kamil.upper().replace(" ", ""))
 
-lista1 = []
-lista2 = [1,2,3,4,5]
-lista3 = list(range(1,6,1))
-lista4 = [1, 2] * 10
-#print(lista4)
+# lista1 = []
+# lista2 = [1,2,3,4,5]
+# lista3 = list(range(1,6,1))
+# lista4 = [1, 2] * 10
+# #print(lista4)
+#
+# #print("Witaj świecie!\n" * 10)
+#
+# lista = [2,3,4,5,3]
+# lista2 = ['kamil', 'ślimak']
+# print(lista)
+# lista.append(6)             #dodaje element na konec lista
+# print(lista)
+# lista.insert(2, "ala")  #dodaje element pod wskazany index
+# print(lista)
+# lista.extend(lista2)        #dodaje inną listę do list
+# print(lista)
+# lista.remove(3)             #kasuje pierwsze wystąpienie elementu
+# print(lista)
+# lista.pop()                 #kasuje ostatni element
+# print(lista)
+# lista.pop(1)                #kasuje element o indexie
+# print(lista)
+# lista.clear()               #czyści listę
+# print(lista)
+#
+# lista = [5,2,-5,4,99,0]
+# print(lista)
+# lista.sort(reverse=True)    # sortuje listę zgodnie z "matematyką"
+# print(lista)
+# print(len(lista))           # ilośc elementów list
 
-#print("Witaj świecie!\n" * 10)
+# print(lista[-2])
 
-lista = [2,3,4,5,3]
-lista2 = ['kamil', 'ślimak']
-print(lista)
-lista.append(6)             #dodaje element na konec lista
-print(lista)
-lista.insert(2, "ala")  #dodaje element pod wskazany index
-print(lista)
-lista.extend(lista2)        #dodaje inną listę do list
-print(lista)
-lista.remove(3)             #kasuje pierwsze wystąpienie elementu
-print(lista)
-lista.pop()                 #kasuje ostatni element
-print(lista)
-lista.pop(1)                #kasuje element o indexie
-print(lista)
-lista.clear()               #czyści listę
-print(lista)
+# lista3 = [5,2,-5,4,99,0]
+# slownik3 = {0:5, 1:2, 2:-5, 3:4, 4:99, 5:0}
+# print(lista3[4])
+# print(slownik3[4])
+
+# slownik = {"imie": "Bartosz", "nazwisko": "Bryniarski", "adres": "Wrocław"}
+# slownik["wiek"] = 45
+# print(slownik)
+
+# cos = {"a": 3, "PI": 3.14, "lista": [4,3,[4,5,{"t":4,"s":"mmm"},6],3.14,"ala"], 7:"Bartosz"}
+#
+# print(cos["lista"][2][2]["s"])
+
+for a in range(0,11):
+    for b in range(0,11):
+        if a == 0 and b == 0:
+            print("\033[31m*\033[0m", end="\t")
+        elif a == 0:
+            print("\033[31m", b, "\033[0m", sep="", end="\t")
+        elif b == 0:
+            print("\033[31m", a, "\033[0m", sep="", end="\t")
+        elif a == b:
+            print("\033[32m", a*b, "\033[0m", sep="", end="\t")
+        else:
+            print(a*b, end="\t")
+    print()
+
