ae2302add2228739246fa88f680ff0c8c5a9d7b9
Bartosz Bryniarski <brylka@jelenia.com>
2025-05-25 10:43:23 +0200

    niedziela

--- pln.py
+++ pln.py
@@ -6,6 +6,14 @@ response = requests.get(url)
 data = response.json()
 #print(data)
 
-pln = float(input("Podaj kwotę w PLN: "))
-print(f"Przelicznik euro: {data['rates']['EUR']}")
-print(f"Kwota {pln}PLN to {data['rates']['EUR']*pln}EUR")
+# pln = float(input("Podaj kwotę w PLN: "))
+# print(f"Przelicznik euro: {data['rates']['EUR']}")
+# print(f"Kwota {pln}PLN to {data['rates']['EUR']*pln}EUR")
+
+# Dodaj pętlę, która wyświetli wszystkie przyliczniki
+
+for klucz, wartosc in data['rates'].items():
+    print(f"Waluta {klucz} przelicznik {wartosc}")
+
+# for key, value in data['rates'].items():
+#     print(key, value)
