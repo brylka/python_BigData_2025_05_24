1d3935c689ef30b3d045ea80f32f3bf876272779
Bartosz Bryniarski <brylka@jelenia.com>
2025-05-25 09:34:38 +0200

    niedziela

--- car.py
+++ car.py
@@ -4,6 +4,8 @@ class Samochod:
         self._kolor = kolor
         self.predkosc = 0
         print(f"Samochód {self.marka} koloru {self._kolor} wita")
+    def __str__(self):
+        return f"Samochód {self.marka} koloru {self._kolor}"
     def przedstaw_sie(self):
         print(f"Jestem samochodem {self.marka}")
     def przyspiesz(self):
@@ -23,24 +25,47 @@ class Samochod:
     def setter_kolor(self, nowy_kolor):
         self._kolor = nowy_kolor
 
+samochody = []
+while True:
+    co = input("Co chcesz zrobić? (1 - stworzyć samochód, 2 - wyświetl samochody, 3 - interakcja z samochodem)")
+    if co == "1":
+        marka = input("Podaj markę: ")
+        kolor = input("Podaj kolor: ")
+        samochod = Samochod(marka, kolor)
+        samochody.append(samochod)
+    elif co == "2":
+        for samochod in samochody:
+            print(samochod)
+    elif co == "3":
+        ktory = int(input(f"Który samochód 0-{len(samochody)-1}"))
+        akcja = input("1 - przyspiesz, 2 - zwolnij, 3 - klakson")
+        if akcja == "1":
+            samochody[ktory].przyspiesz()
+        # elif
 
-bmw = Samochod("BMW")
-bmw.przedstaw_sie()
-bmw.przyspiesz()
-bmw.przyspiesz()
 
-mini = Samochod("MINI")
-mini.przyspiesz()
-bmw.przyspiesz()
-mini.hamuj()
-mini.hamuj()
-mini.klakson()
 
-print(mini.getter_kolor())
-mini.setter_kolor("czerwony")
-print(mini.getter_kolor())
 
-print("="*30)
-print(mini._kolor)
-mini._kolor = "Niebieski"
-print(mini._kolor)
+
+
+
+# bmw = Samochod("BMW")
+# bmw.przedstaw_sie()
+# bmw.przyspiesz()
+# bmw.przyspiesz()
+#
+# mini = Samochod("MINI")
+# mini.przyspiesz()
+# bmw.przyspiesz()
+# mini.hamuj()
+# mini.hamuj()
+# mini.klakson()
+#
+# print(mini.getter_kolor())
+# mini.setter_kolor("czerwony")
+# print(mini.getter_kolor())
+#
+# print("="*30)
+# print(mini._kolor)
+# mini._kolor = "Niebieski"
+# print(mini._kolor)
